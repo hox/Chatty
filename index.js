15196fa4a65a86523bcc82867de442e5251c4516
Caeser <ehernandez@evistaresources.com>
2018-11-14 18:04:39 -0500

    Socket.IO? Here we come..
    Co-Authored-By: Luke Whrit <324luke@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,25 +2,43 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io').listen(http);
+
+// ! REDESIGN
+
+var lukeserverapp = express();
+var lukeserverhttp = require('http').Server(lukeserverapp);
+lukeserverapp.use(express.static(__dirname + '/public new'));
+
+lukeserverapp.get('/', function (req, res) {
+    res.sendFile(__dirname + "/public new/index.html")
+})
+
+lukeserverhttp.listen(81, function () {
+    console.log("lukeserverhttp now listening to port 81");
+});
+
+// ! REDESIGN
+
+const PORT = 80;
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+http.listen(PORT, function () {
+    console.log(`Now listening to * on port ${PORT}`);
 });
 
 io.on('connection', function (socket) {
-    socket.on('SIGNIN', function (msg) {
-        console.log(msg);
-    });
-    socket.on('SIGNUP', function (msg) {
-        console.log(msg);
-    });
     socket.on('MESSAGE', function (msg) {
-        io.emit('chat message', msg);
-        console.log(msg)
+        var json = {};
+        try {
+            json = JSON.parse(msg);
+        } catch (e) {
+            return;
+        }
+        console.log(msg);
     });
 });
--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,19 +1,32 @@
 var socket = io();
 
-/*var wss = new WebSocket("wss://chattyapp.xyz:35012");
-
 if (token() != null) window.location.href = "./chat.html";
+
+function token() {
+    var name = "token";
+    var url = window.location.href + "?" + document.cookie;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
 
 function loginSubmit() {
     var usernamekey = document.getElementById("usernamekey").value;
     var passwordkey = document.getElementById("passwordkey").value;
-    wss.send(JSON.stringify({
+    socket.emit('MESSAGE', {
         "TYPE": "SIGNIN",
         "USERNAME": usernamekey,
         "PASSWORD": passwordkey
-    }));
+    });
     setTimeout(function () {}, 2000);
 }
+
+
+/* 
+! var wss = new WebSocket("wss://chattyapp.xyz:35012");
 
 wss.addEventListener("message", function (message) {
     var json = JSON.parse(message.data);
@@ -29,15 +42,4 @@ wss.addEventListener("message", function (message) {
             window.location.href = "./chat.html";
         }
     }
-});
-
-function token() {
-    var name = "token";
-    var url = window.location.href + "?" + document.cookie;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}*/
+});*/
