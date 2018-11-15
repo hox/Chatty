5e9354cc9d95d883f9f376ef490f1fbef94816a7
Caeser <ehernandez@evistaresources.com>
2018-11-14 20:12:56 -0500

    pushie woop
    Co-Authored-By: Luke Whrit <324luke@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+//! hit or miss, i guess they never miss, huh?
 var express = require('express');
 var app = express();
+var rtg = require('random-token-generator');
 var http = require('http').Server(app);
 var io = require('socket.io').listen(http);
+var SHA256 = require('js-sha256').sha256;
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./data/users.db');
 
-// ! REDESIGN
+db.run(`CREATE TABLE IF NOT EXISTS Users(USERNAME text, PASSWORD text, ADMIN text, TOKEN text)`);
+
+/* ! REDESIGN
 
 var lukeserverapp = express();
 var lukeserverhttp = require('http').Server(lukeserverapp);
@@ -14,22 +21,34 @@ lukeserverapp.get('/', function (req, res) {
 })
 
 lukeserverhttp.listen(81, function () {
-    console.log("lukeserverhttp now listening to port 81");
+    console.log("luke's http server now listening to port 81");
 });
 
-// ! REDESIGN
+*/
 
 const PORT = 80;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public new'));
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public new/index.html');
 });
 
 http.listen(PORT, function () {
     console.log(`Now listening to * on port ${PORT}`);
 });
+
+setInterval(function () {
+    io.emit("MESSAGE", JSON.stringify({
+        "TYPE": "PING"
+    }));
+    current_alive = [];
+    setTimeout(function () {}, 3000);
+    io.emit("MESSAGE", JSON.stringify({
+        "TYPE": "USERS",
+        "MESSAGE": current_alive
+    }));
+}, 10000);
 
 io.on('connection', function (socket) {
     socket.on('MESSAGE', function (msg) {
@@ -37,8 +56,107 @@ io.on('connection', function (socket) {
         try {
             json = JSON.parse(msg);
         } catch (e) {
+            console.log(e);
             return;
         }
-        console.log(msg);
+        if (json.TYPE == "UPDATE") {
+            if (json.TOKEN == "") return;
+            db.all(`SELECT * from Users`, [], (err, rows) => {
+                if (err) {
+                    throw err;
+                }
+                rows.forEach(function (element) {
+
+                });
+            });
+        }
+        if (json.TYPE == "MESSAGE") {
+            var newmessage = json.MESSAGE.replaceAll("<", "&lt").replaceAll(">", "&gt;").trim();
+            refreshDb();
+            db.all(`SELECT * from Users`, [], function (err, rows) {
+                if (err) {
+                    throw err;
+                }
+                rows.forEach(function (element) {
+                    if (element.TOKEN == json.TOKEN) {
+                        io.emit("MESSAGE", JSON.stringify({
+                            "TYPE": "MESSAGE",
+                            "USERNAME": element.USERNAME,
+                            "MESSAGE": newmessage,
+                            "CHANNEL": json.CHANNEL
+                        }));
+                        writeMessage(element.USERNAME, json.MESSAGE, json.CHANNEL);
+                    }
+                });
+            });
+
+        }
+        if (json.TYPE == "SIGNIN") {
+            var oof = false;
+            db.all(`SELECT * from Users`, [], function (err, rows) {
+                if (err) {
+                    throw err;
+                }
+                rows.forEach(function (element) {
+                    if (element.USERNAME == json.USERNAME && element.PASSWORD == SHA256(json.PASSWORD)) {
+                        socket.emit("MESSAGE", JSON.stringify({
+                            "TYPE": "SIGNIN",
+                            "MESSAGE": element.TOKEN
+                        }));
+                        oof = true;
+                    }
+                });
+            });
+            if (!oof)
+                return socket.emit("MESSAGE", JSON.stringify({
+                    "TYPE": "SIGNIN",
+                    "MESSAGE": "USERDATA_INVALID"
+                }));
+
+        }
+        if (json.TYPE == "SIGNUP") {
+            db.all("select * from Users", [], function (err, rows) {
+                if (err) {
+                    throw err;
+                }
+                rows.forEach(function (element) {
+                    if (element.USERNAME == json.USERNAME) {
+                        socket.emit("MESSAGE", JSON.stringify({
+                            "TYPE": "SIGNUP",
+                            "MESSAGE": "USERNAME_TAKEN"
+                        }));
+                        return;
+                    }
+                });
+                rtg.generateKey({
+                    len: 32,
+                    string: true,
+                    strong: true,
+                    retry: true
+                }, function (err, key) {
+                    db.run(`insert into Users values('${json.USERNAME}', '${SHA256(json.PASSWORD)}', false, '${key}')`);
+                });
+            });
+        }
+        if (json.TYPE == "MESSAGES") {
+            var channel = json.CHANNEL;
+            if (channel == "") return;
+            var msgarray = [];
+            fs.readdir("./data/logs/", function (err, filename) {
+                if (filename == channel + ".json") {
+                    fs.readFile("./data/logs/" + filename, "utf8", function (err, data) {
+                        var json = JSON.parse(data);
+                        for (i = 0; i < 10; i++) {
+                            var message = json.logs[i];
+                            msgarray.unshift(message);
+                        }
+                        socket.emit("MESSAGE", JSON.stringify({
+                            "TYPE": "MESSAGES",
+                            "MESSAGE": msgarray
+                        }));
+                    });
+                }
+            });
+        }
     });
 });
---
+++ public new/js/signup.js
@@ -0,0 +1,41 @@
+var socket = io();
+
+if (token() != null) window.location.href = "../chat/";
+
+function token() {
+    var name = "token";
+    var url = window.location.href + "?" + document.cookie;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function loginSubmit() {
+    var usernamekey = document.getElementById("usernamekey").value;
+    var passwordkey = document.getElementById("passwordkey").value;
+    socket.emit('MESSAGE', JSON.stringify({
+        "TYPE": "SIGNUP",
+        "USERNAME": usernamekey,
+        "PASSWORD": passwordkey
+    }));
+    setTimeout(function () {}, 2000);
+}
+
+socket.on('MESSAGE', function (msg) {
+    var json = JSON.parse(msg);
+    if (json.TYPE == "SIGNUP") {
+        if (json.MESSAGE == "USERNAME_TAKEN") {
+            document.getElementById("userwrong").style = "";
+        } else {
+            console.log(json.MESSAGE)
+            var exdate = new Date();
+            exdate.setDate(exdate.getDate() + 1440);
+            var c_value = escape(json.MESSAGE) + ((1440 == null) ? "" : "; expires=" + exdate.toUTCString());
+            document.cookie = "token=" + c_value + "; path=/";
+            window.location.href = "./chat/";
+        }
+    }
+});
